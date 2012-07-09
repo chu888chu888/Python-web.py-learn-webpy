091412d1e94c89177d165c13e21866f94af15790
lexdene@sohu.com <lexdene@sohu.com@ffde0ac6-e832-4f71-981b-a56168ce4078>
2012-07-09 08:31:23 +0000

    a
    git-svn-id: https://svn.sinaapp.com/trypython@23 ffde0ac6-e832-4f71-981b-a56168ce4078

--- app/route.py
+++ app/route.py
@@ -7,11 +7,16 @@ render = web.template.render(templates_root)
 class route:
     def GET(self,path):
         print "path is %s ;"%path
-        c = 'app.controller.' + path.replace('/','.');
-        print "c = %s ;" % c
+        modPath = 'app.controller.' + path.replace('/','.');
+        print "modPath = %s ;" % modPath
+        className = modPath[ modPath.rfind('.')+1 : ]
+        print "className = %s;" % className
         try:
-            mod = __import__(c)
+            mod = __import__(modPath)
             print mod
+            c = getattr(mod,className)
+            obj = c()
+            print obj
         except ImportError:
             print 'import error'
         return render.hello()
