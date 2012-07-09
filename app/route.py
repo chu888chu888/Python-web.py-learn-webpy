06689deca530d6d3d198dba276668815ba6c159c
lexdene@sohu.com <lexdene@sohu.com@ffde0ac6-e832-4f71-981b-a56168ce4078>
2012-07-09 09:56:00 +0000

    a
    git-svn-id: https://svn.sinaapp.com/trypython@31 ffde0ac6-e832-4f71-981b-a56168ce4078

--- app/route.py
+++ app/route.py
@@ -4,6 +4,9 @@ app_root = os.path.dirname(__file__)
 templates_root = os.path.join(app_root, 'template')
 render = web.template.render(templates_root)
 
+
+import app.controller
+
 class route:
     def GET(self,path):
         print "path is %s ;"%path
