58019a510e60e9a4798d442d1832ae714b3f6227
lexdene <lexdene@sohu.com>
2012-11-13 18:15:05 +0800

    Permission Denied for UnjudgeSubmitList

--- app/controller/judgeface/UnjudgeSubmitList.py
+++ app/controller/judgeface/UnjudgeSubmitList.py
@@ -4,6 +4,10 @@ from controller.base.Controller import Controller
 import model
 
 class UnjudgeSubmitList(Controller):
+	s_config = {
+		'title':u'还没有结果的提交列表',
+		'permission':'judge',
+	}
 	def process(self):
 		aSubmitModel = model.Model.Model('submit')
 		aIter = aSubmitModel.select(
@@ -11,3 +15,10 @@ class UnjudgeSubmitList(Controller):
 		)
 		
 		self.setVariable('aIter',aIter)
+		
+		self.setVariable('success',True)
+		self.setVariable('result','')
+		
+	def permissionDenied(self):
+		self.setVariable('success',False)
+		self.setVariable('result','permissionDenied')
